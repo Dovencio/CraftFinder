0aad327dd19493226908b6c6d2afab128467b58a
moute3 <athin@192.168.1.252>
2020-08-22 03:17:58 -0600

    Fixed beans.

--- src/main/java/net/deathcon/craftfinder/item/Armor1runestoneItem.java
+++ src/main/java/net/deathcon/craftfinder/item/Armor1runestoneItem.java
@@ -1,12 +1,25 @@
 
 package net.deathcon.craftfinder.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.deathcon.craftfinder.itemgroup.RunesItemGroup;
+import net.deathcon.craftfinder.CraftfinderModElements;
+
+import java.util.List;
+
 @CraftfinderModElements.ModElement.Tag
 public class Armor1runestoneItem extends CraftfinderModElements.ModElement {
-
 	@ObjectHolder("craftfinder:armor_1runestone")
 	public static final Item block = null;
-
 	public Armor1runestoneItem(CraftfinderModElements instance) {
 		super(instance, 2);
 	}
@@ -15,9 +28,7 @@ public class Armor1runestoneItem extends CraftfinderModElements.ModElement {
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
-
 	public static class ItemCustom extends Item {
-
 		public ItemCustom() {
 			super(new Item.Properties().group(RunesItemGroup.tab).maxStackSize(64));
 			setRegistryName("armor_1runestone");
@@ -43,7 +54,5 @@ public class Armor1runestoneItem extends CraftfinderModElements.ModElement {
 			super.addInformation(itemstack, world, list, flag);
 			list.add(new StringTextComponent("Adds 1 AC to armor"));
 		}
-
 	}
-
 }
--- src/main/java/net/deathcon/craftfinder/itemgroup/RunesItemGroup.java
+++ src/main/java/net/deathcon/craftfinder/itemgroup/RunesItemGroup.java
@@ -1,9 +1,17 @@
 
 package net.deathcon.craftfinder.itemgroup;
 
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+
+import net.deathcon.craftfinder.item.UnmarkedrunstoneItem;
+import net.deathcon.craftfinder.CraftfinderModElements;
+
 @CraftfinderModElements.ModElement.Tag
 public class RunesItemGroup extends CraftfinderModElements.ModElement {
-
 	public RunesItemGroup(CraftfinderModElements instance) {
 		super(instance, 3);
 	}
@@ -23,7 +31,5 @@ public class RunesItemGroup extends CraftfinderModElements.ModElement {
 			}
 		};
 	}
-
 	public static ItemGroup tab;
-
 }
