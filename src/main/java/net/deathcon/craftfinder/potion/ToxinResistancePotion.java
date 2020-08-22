75dd017a4456428c4a6189c159d696742dae75a3
Dovencio <dovencio.doshorte@gmail.com>
2020-08-22 03:32:06 -0600

    MCreator Merge commit 'dfa226687fe96a111dad5799d6159683d2d429de' into 35a4a06da1797dbcd623f73d6e8a7e40c5688942

--- src/main/java/net/deathcon/craftfinder/potion/ToxinResistancePotion.java
+++ src/main/java/net/deathcon/craftfinder/potion/ToxinResistancePotion.java
@@ -1,27 +1,18 @@
 
 package net.deathcon.craftfinder.potion;
 
-import net.minecraftforge.registries.ObjectHolder;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.RegistryEvent;
-
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.EffectType;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effect;
-
-import net.deathcon.craftfinder.CraftfinderModElements;
-
 @CraftfinderModElements.ModElement.Tag
 public class ToxinResistancePotion extends CraftfinderModElements.ModElement {
+
 	@ObjectHolder("craftfinder:toxin_resistance")
 	public static final Effect potion = null;
+
 	@ObjectHolder("craftfinder:toxin_resistance")
 	public static final Potion potionType = null;
+
 	public ToxinResistancePotion(CraftfinderModElements instance) {
 		super(instance, 12);
+
 		FMLJavaModLoadingContext.get().getModEventBus().register(this);
 	}
 
@@ -34,15 +25,20 @@ public class ToxinResistancePotion extends CraftfinderModElements.ModElement {
 	public void registerPotion(RegistryEvent.Register<Potion> event) {
 		event.getRegistry().register(new PotionCustom());
 	}
+
 	public static class PotionCustom extends Potion {
+
 		public PotionCustom() {
 			super(new EffectInstance(potion, 3600));
 			setRegistryName("toxin_resistance");
 		}
+
 	}
 
 	public static class EffectCustom extends Effect {
+
 		private final ResourceLocation potionIcon;
+
 		public EffectCustom() {
 			super(EffectType.BENEFICIAL, -16764109);
 			setRegistryName("toxin_resistance");
@@ -83,5 +79,7 @@ public class ToxinResistancePotion extends CraftfinderModElements.ModElement {
 		public boolean isReady(int duration, int amplifier) {
 			return true;
 		}
+
 	}
+
 }
