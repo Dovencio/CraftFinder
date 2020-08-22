dfa226687fe96a111dad5799d6159683d2d429de
athin <athin@192.168.1.252>
2020-08-22 03:17:59 -0600

    MCreator Merge commit '5138c4d111df83364a01b796b4221a00cc3c01b0' into 0aad327dd19493226908b6c6d2afab128467b58a

--- src/main/java/net/deathcon/craftfinder/item/Armor1runestoneItem.java
+++ src/main/java/net/deathcon/craftfinder/item/Armor1runestoneItem.java
@@ -1,25 +1,12 @@
 
 package net.deathcon.craftfinder.item;
 
-import net.minecraftforge.registries.ObjectHolder;
-
-import net.minecraft.world.World;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Item;
-import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.block.BlockState;
-
-import net.deathcon.craftfinder.itemgroup.RunesItemGroup;
-import net.deathcon.craftfinder.CraftfinderModElements;
-
-import java.util.List;
-
 @CraftfinderModElements.ModElement.Tag
 public class Armor1runestoneItem extends CraftfinderModElements.ModElement {
+
 	@ObjectHolder("craftfinder:armor_1runestone")
 	public static final Item block = null;
+
 	public Armor1runestoneItem(CraftfinderModElements instance) {
 		super(instance, 2);
 	}
@@ -28,7 +15,9 @@ public class Armor1runestoneItem extends CraftfinderModElements.ModElement {
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
+
 	public static class ItemCustom extends Item {
+
 		public ItemCustom() {
 			super(new Item.Properties().group(RunesItemGroup.tab).maxStackSize(64));
 			setRegistryName("armor_1runestone");
@@ -54,5 +43,7 @@ public class Armor1runestoneItem extends CraftfinderModElements.ModElement {
 			super.addInformation(itemstack, world, list, flag);
 			list.add(new StringTextComponent("Adds 1 AC to armor"));
 		}
+
 	}
+
 }
--- src/main/java/net/deathcon/craftfinder/item/Weapon1runestoneItem.java
+++ src/main/java/net/deathcon/craftfinder/item/Weapon1runestoneItem.java
@@ -1,19 +1,12 @@
 
 package net.deathcon.craftfinder.item;
 
-import net.minecraftforge.registries.ObjectHolder;
-
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Item;
-import net.minecraft.block.BlockState;
-
-import net.deathcon.craftfinder.itemgroup.RunesItemGroup;
-import net.deathcon.craftfinder.CraftfinderModElements;
-
 @CraftfinderModElements.ModElement.Tag
 public class Weapon1runestoneItem extends CraftfinderModElements.ModElement {
+
 	@ObjectHolder("craftfinder:weapon_1runestone")
 	public static final Item block = null;
+
 	public Weapon1runestoneItem(CraftfinderModElements instance) {
 		super(instance, 4);
 	}
@@ -22,7 +15,9 @@ public class Weapon1runestoneItem extends CraftfinderModElements.ModElement {
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
+
 	public static class ItemCustom extends Item {
+
 		public ItemCustom() {
 			super(new Item.Properties().group(RunesItemGroup.tab).maxStackSize(64));
 			setRegistryName("weapon_1runestone");
@@ -42,5 +37,7 @@ public class Weapon1runestoneItem extends CraftfinderModElements.ModElement {
 		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 			return 1F;
 		}
+
 	}
+
 }
